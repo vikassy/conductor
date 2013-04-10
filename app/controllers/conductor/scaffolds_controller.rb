a222fa1e95c5504393fc513e12eacb518eebe900
vikas <vikasyaligar.it@gmail.com>
2013-04-10 16:34:03 +0530

    Returning array in scaffold generator

--- app/controllers/conductor/scaffolds_controller.rb
+++ app/controllers/conductor/scaffolds_controller.rb
@@ -14,12 +14,14 @@ module Conductor
   
     private
       def generator_arguments
-        Array.new do |args|
+        array = Array.new
+        array do |args|
           args << params[:scaffold][:name]
           args << "--skip-timestamps" if params[:scaffold][:skip_timestamps] == "1"
           args << "--skip-migration" if params[:scaffold][:skip_migration] == "1"
           args << params[:scaffold][:fields].select { |f| f[:name].blank? }.collect { |f| "#{f[:name]}:#{f[:type]}" }
         end
+        return array
       end
   end
 end
